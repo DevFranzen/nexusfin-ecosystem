56c21e0b97f10b57cfbd57cccd57a6fc827d0c07
thomas.franzen <thomas.franzen@adesso.de>
2026-02-11 11:40:38 +0100

    Initial setup

---
+++ apps/exchange/ex-world-engine/src/main/java/de/pamunda/nexusfin/exchange/ex_world_engine/ExWorldEngineApplication.java
@@ -0,0 +1,13 @@
+package de.pamunda.nexusfin.exchange.ex_world_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExWorldEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExWorldEngineApplication.class, args);
+	}
+
+}
